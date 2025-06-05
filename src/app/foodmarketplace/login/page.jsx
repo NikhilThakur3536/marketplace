c8f28e33e493545cdcfd8b13cb3f09b8d888451b
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-06-05 14:40:36 +0530

    domain Id vlaue error fixed

--- src/app/foodmarketplace/login/page.jsx
+++ src/app/foodmarketplace/login/page.jsx
@@ -2,7 +2,7 @@ import LoginComponent from "@/app/components/Login";
 
 export default function Login() {
     
-    const DOMAINID = process.env.NEXT_PUBLIC_CDOMAIN_ID
+    const DOMAINID = process.env.NEXT_PUBLIC_DOMAIN_ID
     const DEVICEID = process.env.NEXT_PUBLIC_DEVICE_ID 
     const DEVICETOKEN = process.env.NEXT_PUBLIC_DEVICE_TOKEN
     
