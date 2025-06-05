2331ae7b752438a6238fa016061cf8190b38a87f
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-06-05 14:32:43 +0530

    selection page and food marketplace login set up

---
+++ src/app/foodmarketplace/login/page.jsx
@@ -0,0 +1,12 @@
+import LoginComponent from "@/app/components/Login";
+
+export default function Login() {
+    
+    const DOMAINID = process.env.NEXT_PUBLIC_CDOMAIN_ID
+    const DEVICEID = process.env.NEXT_PUBLIC_DEVICE_ID 
+    const DEVICETOKEN = process.env.NEXT_PUBLIC_DEVICE_TOKEN
+    
+    return(
+        <LoginComponent showCredentialsLogin={false} redirectPath={"/foodmarketplace"} domainId={DOMAINID} deviceId={DEVICEID} deviceToken={DEVICETOKEN}/>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/navigation';
+import styles from '../globals.css';
+
+export default function MarketplaceSelection() {
+  const router = useRouter();
+
+  const handleNavigation = (marketplace) => {
+    router.push(`/marketplace/${marketplace}/login`);
+  };
+
+  return (
+    <div className="container">
+      <h1>Select Your Marketplace</h1>
+      <div className="buttonContainer">
+        <button
+          onClick={() => handleNavigation('foodmarketplace')}
+          className="marketplaceButton"
+        >
+          Food Marketplace
+        </button>
+        <button
+          onClick={() => handleNavigation('electronicsmarketplace')}
+          className="marketplaceButton"
+        >
+          Electronics Marketplace
+        </button>
+        <button
+          onClick={() => handleNavigation('automarketplace')}
+          className="marketplaceButton"
+        >
+          Auto Marketplace
+        </button>
+      </div>
+    </div>
+  );
+}
