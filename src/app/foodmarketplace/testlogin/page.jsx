aa13796470a4e6ebe838b95e6846305bdbc0f2b1
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-06-17 16:15:12 +0530

    sign and login ui changes for food and eletronic marketplace

--- src/app/foodmarketplace/testlogin/page.jsx
+++ src/app/foodmarketplace/testlogin/page.jsx
@@ -1,77 +1,86 @@
 "use client"
 
-import Image from "next/image";
+import { Lock, Mail } from "lucide-react";
 import { motion } from "framer-motion";
-import { Lock, Mail, UserRound } from "lucide-react";
+
 
 export default function TestLogin () {
     return(
-        <div className="max-w-screen flex justify-center min-h-screen">
-            <div className="w-full max-w-md relative bg-[#E6E6E6] overflow-hidden flex flex-col justify-center items-center gap-5">
-                <Image src={"/foodbg.png"} fill alt=" bgimage" className="absolute object-center opacity-65"/>
-                <svg width="424" height="195" viewBox="0 0 424 195" fill="none" xmlns="http://www.w3.org/2000/svg" className="z-20 absolute -right-6 -top-2">
-                    <g filter="url(#filter0_d_601_54)">
-                    <path d="M406.495 163.579C406.495 163.579 306.779 91.8225 233 69.5787C169.741 50.5068 119.873 86.3799 64.9989 49.5788C43.3725 35.075 17.1767 3.11295 17.1767 3.11295L406.495 3.11294L406.495 163.579Z" fill="url(#paint0_linear_601_54)"/>
+        <div className="w-screen flex justify-center min-h-screen">
+            <div className="w-full max-w-md overflow-hidden bg-gray-100 relative flex flex-col items-center justify-center gap-5">
+                <div className={`absolute -inset-1 w-full bg-[radial-gradient(circle,_#9ca3af_1px,_transparent_1px)] bg-[size:10px_10px] opacity-30`}></div>        
+                <svg width="111" height="102" viewBox="0 0 111 102" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute left-0 top-0">
+                    <g filter="url(#filter0_d_612_71)">
+                    <path d="M93.5 0C93.5 0 95.1867 62.4809 67 69.5C50.778 73.5396 42.0473 56.6209 25.5 59C14.8401 60.5326 0 69.5 0 69.5V0H93.5Z" fill="url(#paint0_linear_612_71)"/>
                     </g>
                     <defs>
-                    <filter id="filter0_d_601_54" x="0.176758" y="0.112915" width="423.318" height="194.466" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <filter id="filter0_d_612_71" x="-17" y="-3" width="127.52" height="104.126" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
                     <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                     <feOffset dy="14"/>
                     <feGaussianBlur stdDeviation="8.5"/>
                     <feComposite in2="hardAlpha" operator="out"/>
                     <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_601_54"/>
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_601_54" result="shape"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_612_71"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_612_71" result="shape"/>
                     </filter>
-                    <linearGradient id="paint0_linear_601_54" x1="264.911" y1="-52.2897" x2="202.89" y2="106.208" gradientUnits="userSpaceOnUse">
+                    <linearGradient id="paint0_linear_612_71" x1="46.7596" y1="0" x2="46.7596" y2="70.1262" gradientUnits="userSpaceOnUse">
                     <stop stopColor="#F98C37"/>
                     <stop offset="1" stopColor="#EE6416"/>
                     </linearGradient>
                     </defs>
                 </svg>
-                <svg width="355" height="164" viewBox="0 0 355 164" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute -bottom-4 -left-4">
-                    <g filter="url(#filter0_d_601_56)">
-                    <path d="M335.168 154.53C335.168 154.53 298.785 48.3001 240.5 28.5001C176.587 6.78826 140.763 109.538 77.5006 86C48.8235 75.33 15.1675 38.53 15.1675 38.53L15.1675 154.53L335.168 154.53Z" fill="url(#paint0_linear_601_56)"/>
+                <svg width="83" height="71" viewBox="0 0 83 71" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute right-0 top-0">
+                    <g filter="url(#filter0_d_612_74)">
+                    <path d="M46.5 34C33.6127 26.3848 30.1421 7.18477 17.2548 -0.430453L83.2548 -0.430451L83.2548 38.5695C83.2548 38.5695 59.3873 41.6152 46.5 34Z" fill="url(#paint0_linear_612_74)"/>
                     </g>
                     <defs>
-                    <filter id="filter0_d_601_56" x="0.0679684" y="0.366186" width="354.2" height="163.264" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <filter id="filter0_d_612_74" x="0.253906" y="-3.43048" width="100" height="73.6016" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset dy="14"/>
+                    <feGaussianBlur stdDeviation="8.5"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_612_74"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_612_74" result="shape"/>
+                    </filter>
+                    <linearGradient id="paint0_linear_612_74" x1="67.3359" y1="-9.83704" x2="42.7438" y2="31.7804" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#F98C37"/>
+                    <stop offset="1" stopColor="#EE6416"/>
+                    </linearGradient>
+                    </defs>
+                </svg>
+                <svg width="338" height="212" viewBox="0 0 338 212" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute bottom-0 right-0">
+                    <g filter="url(#filter0_d_613_79)">
+                    <path d="M338.478 219.085L15.4776 219.085C15.4776 219.085 38.6963 160.259 67.2207 133.656C95.5138 107.269 109.64 100.035 148.221 97.1561C215.221 92.1561 237.193 98.7041 280.721 75.6561C307.216 61.6271 338.478 25.0847 338.478 25.0847L338.478 219.085Z" fill="url(#paint0_linear_613_79)"/>
+                    </g>
+                    <defs>
+                    <filter id="filter0_d_613_79" x="0.477539" y="0.0847168" width="357.001" height="228" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
                     <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                     <feOffset dx="2" dy="-8"/>
-                    <feGaussianBlur stdDeviation="8.55"/>
+                    <feGaussianBlur stdDeviation="8.5"/>
                     <feComposite in2="hardAlpha" operator="out"/>
                     <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_601_56"/>
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_601_56" result="shape"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_613_79"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_613_79" result="shape"/>
                     </filter>
-                    <linearGradient id="paint0_linear_601_56" x1="204.557" y1="15.4565" x2="138.001" y2="199.057" gradientUnits="userSpaceOnUse">
+                    <linearGradient id="paint0_linear_613_79" x1="150.5" y1="78" x2="262.607" y2="264.654" gradientUnits="userSpaceOnUse">
                     <stop stopColor="#F98C37"/>
                     <stop offset="1" stopColor="#EE6416"/>
                     </linearGradient>
                     </defs>
                 </svg>
-                <h1 className="text-4xl font-bold ">Get On Board !</h1>
+                <h1 className="text-4xl font-bold w-[70%] text-center">Hello Welcome Back !</h1>
                 <form  className="max-w-md w-full p-4 z-30 flex flex-col justify-center items-center px-12 gap-5">
-                    <div className="w-[80%] relative h-fit">
-                        <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl bg-white h-12  focus:outline-2 focus:outline-[#F98C37]"
-                        required
-                        />
-                        <div className="flex items-center justify-center p-2 rounded-xl bg-gradient-to-r from-[#F98C37]  to-[#EE6416] absolute top-2.5 left-1 ">
-                            <UserRound color="white" size={20} />
-                        </div>
-                    </div>
-
                     <div className="w-[80%] relative">
                         <input
                         type="email"
                         id="email"
                         name="email"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl bg-white h-12 focus:outline-2 focus:outline-[#F98C37]"
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl bg-white h-12 focus:outline-2 focus:outline-[#F98C37] pl-12"
+                        placeholder="Enter Identifier"
                         required
                         />
                         <div className="flex items-center justify-center p-2 rounded-xl bg-gradient-to-r from-[#F98C37]  to-[#EE6416] absolute top-2.5 left-1 ">
@@ -79,12 +88,13 @@ export default function TestLogin () {
                         </div>
                     </div>
 
-                     <div className="w-[80%] relative ">
+                        <div className="w-[80%] relative ">
                         <input
                         type="password"
                         id="password"
                         name="password"
-                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl bg-white h-12  focus:outline-2 focus:outline-[#F98C37]"
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl bg-white h-12  focus:outline-2 focus:outline-[#F98C37] pl-12"
+                        placeholder="Enter you Password"
                         required
                         />
                         <div className="flex items-center justify-center p-2 rounded-xl bg-gradient-to-r from-[#F98C37]  to-[#EE6416] absolute top-2.5 left-1 ">
